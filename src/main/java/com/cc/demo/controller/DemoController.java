a9db4b90f9f72569c4e190aedb38c2cbf5cf4789
chenqf <qifeng.chen1006@gmail.com>
2015-09-10 15:20:22 +0800

    增加logback

--- src/main/java/com/cc/demo/controller/DemoController.java
+++ src/main/java/com/cc/demo/controller/DemoController.java
@@ -2,6 +2,8 @@ package com.cc.demo.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +18,8 @@ import com.chenqf.service.UserService;
 @RequestMapping("demo")
 public class DemoController extends BaseController {
 	
+	private static Logger logger = LoggerFactory.getLogger(DemoController.class);
+	
 	@Autowired
 	private UserService userService;
 	
@@ -23,6 +27,9 @@ public class DemoController extends BaseController {
 	@ResponseBody
 	public List<UserDto> queryUsers() throws Exception {
 		
+		logger.info("测试 log4j info");
+		logger.debug("测试 log4j debug");
+		System.getProperty("catalina.home");
 		return this.userService.queryUsers();
 	}
 	
